ac5322f10045906b96747a5d2673616d24948e58
tiempo_glb <1194832791_glb@qq.com>
2021-07-30 07:27:58 +0800

    IDEA first commit ALW

--- src/main/java/com/example/GitTest.java
+++ src/main/java/com/example/GitTest.java
@@ -13,12 +13,9 @@ public class GitTest {
         System.out.println("hot fix add hello git5!");
         System.out.println("hot fix add hello git6!");
         System.out.println("push test git!");
-        System.out.println("push teswt git!");
-        System.out.println("push test1 git!");
         System.out.println("push test in web git!");
+        System.out.println("push test in IDEAALW git!");
         System.out.println("push test in IDEA git!");
-        System.out.println("push test in IDEA in c2 git!");
-        System.out.println("push test in c2 git!");
-        System.out.println("push tfgDEA in c2 git!");
+        System.out.println("push test in IDEAALW git!");
     }
 }
